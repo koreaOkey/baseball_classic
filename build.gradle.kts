0be80e046fee9e153d23b86916ebc2a7479bc43b
koreaOkey <leeyoungje99@gmail.com>
2026-03-30 19:13:56 +0900

    ios 완료(3월30일 배포분)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Root build file for BaseHaptic monorepo (mobile + watch)
 plugins {
-    id("com.android.application") version "8.13.2" apply false
+    id("com.android.application") version "8.12.1" apply false
     id("org.jetbrains.kotlin.android") version "2.0.21" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "2.0.21" apply false
